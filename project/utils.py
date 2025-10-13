fd1b918fd17affa561bfa0e9f756da433deabd87
dgarciamur <dgarciamur@sanitas.es>
2025-10-13 22:35:36 +0200

    Validación y algunos modelos

--- project/utils.py
+++ project/utils.py
@@ -1,12 +1,13 @@
 import torch
 import math 
 from torch.optim.lr_scheduler import LambdaLR
+import torch.nn.functional as F
 
 def evaluate_ppl(model, dataloader, loss_fn, device):
     model.eval() # Le dice al modelo que se comporte como inferencia
     total_loss = 0
     with torch.no_grad():
-        for x, y in dataloader:
+        for x, y in dataloader: 
             x, y = x.to(device), y.to(device)
             logits = model(x)
             loss = loss_fn(logits.view(-1, logits.size(-1)), y.view(-1))
@@ -18,3 +19,32 @@ def evaluate_ppl(model, dataloader, loss_fn, device):
 def lr_lambda(step):
     warmup = 1000
     return min((step+1)/warmup, 1.0)
+
+
+def generate_text(model, sp, prompt, max_new_tokens=50, temperature=1.0, top_k=20, context_len=128, device="cpu"):
+
+    model.eval()
+    tokens = sp.encode(prompt, out_type=int)
+    tokens = torch.tensor(tokens, dtype=torch.long, device=device).unsqueeze(0)
+    
+    for _ in range(max_new_tokens):
+        # Limita el contexto a los últimos context_len tokens
+        idx_cond = tokens[:, -context_len:]
+        
+        # Pasa por el modelo
+        with torch.no_grad():
+            logits = model(idx_cond)
+            logits = logits[:, -1, :] / temperature  # solo último token
+        
+        # Top-k sampling (opcional)
+        if top_k is not None:
+            v, _ = torch.topk(logits, top_k)
+            logits[logits < v[:, [-1]]] = -float("inf")
+        
+        probs = F.softmax(logits, dim=-1)
+        next_token = torch.multinomial(probs, num_samples=1)
+        
+        # Añade nuevo token
+        tokens = torch.cat([tokens, next_token], dim=1)
+    
+    return sp.decode(tokens[0].tolist())
--- project/validation.py
+++ project/validation.py
@@ -6,21 +6,34 @@ import torch.nn as nn
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 MODEL_PATH = "./resources/models/gpt_model.pth"
-
+TEST_SET = False
 loss_fn = nn.CrossEntropyLoss()
+context_len = 128
 
-_,_,test_text = read_datasets(True)
+if TEST_SET:
+    _,_,test_text = read_datasets(True)
 
 
-print(f"Longitud corpus test: {len(test_text)} caracteres")
+    print(f"Longitud corpus test: {len(test_text)} caracteres")
 
-test_ids,vocab_size = tokenizador(test_text)
+    test_ids,vocab_size = tokenizador(test_text)
 
-test_dataset = LMWindowDataset(test_ids, context_len=256)  # o 256
+    test_dataset = LMWindowDataset(test_ids, context_len=context_len)  # o 256
 
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False,num_workers=os.cpu_count(),pin_memory=torch.cuda.is_available(),prefetch_factor=2)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False,num_workers=os.cpu_count(),pin_memory=torch.cuda.is_available(),prefetch_factor=2)
+    print(f"Iteracciones: {len(test_loader)}")
+    model = torch.load(MODEL_PATH, map_location=device,weights_only=False)
 
-model = torch.load(MODEL_PATH, map_location="cpu")
+    test_loss, test_ppl = evaluate_ppl(model, test_loader, loss_fn, device)
+    print(f"Test Loss: {test_loss:.4f} | Test Perplexity: {test_ppl:.2f}")
 
-test_loss, test_ppl = evaluate_ppl(model, test_loader, loss_fn, device)
-print(f"Test Loss: {test_loss:.4f} | Test Perplexity: {test_ppl:.2f}")
+else:
+    sp = spm.SentencePieceProcessor(model_file="./resources/models/bpe_model_shakespeare.model")
+
+    model = torch.load(MODEL_PATH, map_location=device,weights_only=False)
+    model.to(device)
+    model.eval()
+
+    prompt = "KING RICHARD:"
+    generated = generate_text(model, sp, prompt, max_new_tokens=100, temperature=0.8, device=device)
+    print(generated)
